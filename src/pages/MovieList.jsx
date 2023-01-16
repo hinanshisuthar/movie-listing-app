bc11581b8f8c6d7089f795114e157bec64a8386f
Hinanshi Suthar <hinanshis4@gmail.com>
2023-01-16 14:41:30 +0530

    Feat: Search functionality with throttling

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,40 +3,42 @@ import {
   addToFavorite,
   fetchMovies,
   removeFromFavorite,
+  updateSearch,
 } from "../features/movieSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 export const MovieList = () => {
-  const { allMovies, favorites } = useSelector((state) => state.movie);
+  const { allMovies, search, favorites } = useSelector((state) => state.movie);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMovies());
-    // eslint-disable-next-line
-  }, []);
+    let timer;
+    timer = setTimeout(() => dispatch(fetchMovies(search)), 1000);
+
+    return () => clearTimeout(timer);
+  }, [search]);
 
   return (
     <div style={{ display: "flex" }}>
       <div>
         <h3>Movies</h3>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => dispatch(updateSearch(e.target.value))}
+        />
         {!allMovies.loading && allMovies.length ? (
           <ul>
             {allMovies.map((movie) => (
               <li key={movie.title}>
                 {movie.title} and {movie.release_date}
                 {favorites.find((fav) => fav.id === movie.id) ? (
-                  <span
-                    className="fav-icon"
-                    onClick={() => dispatch(removeFromFavorite(movie.id))}
-                  >
+                  <span className="fav-icon" onClick={() => dispatch(removeFromFavorite(movie.id))}>
                     <AiFillHeart />
                   </span>
                 ) : (
-                  <span
-                    className="fav-icon"
-                    onClick={() => dispatch(addToFavorite(movie))}
-                  >
+                  <span className="fav-icon" onClick={() => dispatch(addToFavorite(movie))}>
                     <AiOutlineHeart />
                   </span>
                 )}
@@ -53,10 +55,7 @@ export const MovieList = () => {
           {favorites.map((fav) => (
             <li key={fav.title}>
               {fav.title}
-              <span
-                className="fav-icon"
-                onClick={() => dispatch(removeFromFavorite(fav.id))}
-              >
+              <span className="fav-icon" onClick={() => dispatch(removeFromFavorite(fav.id))}>
                 <AiFillHeart />
               </span>
             </li>
